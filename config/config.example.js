d18d6294a2070002ddbc76f2b36507360396e700
Hamchin <k.terada.1211@gmail.com>
2021-04-11 00:12:30 +0900

    Update

--- config/config.example.js
+++ config/config.example.js
@@ -2,3 +2,4 @@ const TWITTER_NOTICE_API_URL = 'https://XXXXXXXXXX.execute-api.ap-northeast-1.am
 const TWITTER_API_URL = 'https://XXXXXXXXXX.execute-api.ap-northeast-1.amazonaws.com/v1';
 const TWITTER_ACCESS_TOKEN = 'XXXXXXXXXX';
 const TWITTER_ACCESS_SECRET = 'XXXXXXXXXX';
+const CUSTOM_TIMELINE_INTERVAL = 0;
